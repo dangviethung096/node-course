219b3ca7c1b6adeddc799b67f9dcb5ca817b91bf
Hung At Home <dangviethung096@gmail.com>
2020-06-19 23:53:59 +0700

    end section 8

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,43 @@
 console.log('Client side javacript side is loaded')
+
+// fetch('http://puzzle.mead.io/puzzle').then ((response) => {
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// })
+
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+// messageOne.textContent = 'Forecast'
+// messageTwo.textContent = 'Location'
+
+weatherForm.addEventListener('submit', (e) => { 
+    e.preventDefault()
+
+    const location = search.value
+
+    const url = 'http://localhost:3000/weather?address=' + location
+
+    messageOne.textContent = 'Loading ...'
+    messageTwo.textContent = ''
+
+    fetch(url).then((response) => {
+        response.json().then(({ error, forecast, location }) => {
+            if (error) {
+                messageOne.textContent = error
+            } else {
+                messageOne.textContent = location
+                messageTwo.textContent = forecast
+            }
+        })
+    })
+
+})
+
+
+
