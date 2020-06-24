502e6e062aea8b8b0fa0807bff5eb237b5c2ab35
Dang Viet Hung <dangviethung096@gmail.com>
2020-06-24 20:27:27 +0700

    20:27 24/06

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,9 +1,45 @@
 // CRUD
 
-const mongodb = require('mongodb')
-const MongoClient = mongdb.MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
 
-const connectionURL = 'mongdb://127.0.0.1:27017'
-const database = ''
 
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+const id = new ObjectID()
+console.log(id.id.length)
+console.log(id.toHexString().length)
+// console.log(id.getTimestamp())
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database')
+    }
+
+    const db = client.db(databaseName)
+
+    // db.collection('users').findOne({ _id: new ObjectID("5ef34df645cfff1fd7338b36") }, (error, user) => {
+    //     if (error) {
+    //         console.log(error)
+    //     }
+
+    //     console.log(user)
+    // })
+
+    // db.collection('users').find({ name: 'Hung' }).toArray((error, users) => {
+    //     console.log(users)
+    // })
+
+    // db.collection('users').find({ name: 'Hung' }).count((error, count) => {
+    //     console.log(count)
+    // })
+
+    db.collection('tasks').findOne({ _id: new ObjectID('5ef34d02894afd1ea5c07926') }, (error, user) => {
+        console.log(user)
+    })
+
+    db.collection('tasks').find({ completed: true }).toArray((error, users) => {
+        console.log(users)
+    })
+})
 
