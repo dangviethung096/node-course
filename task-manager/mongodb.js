c2091a754bba0d1d7f46b9b870de0b0707f8bbb8
Dang Viet Hung <dangviethung096@gmail.com>
2020-06-26 19:28:11 +0700

    delete comment

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -18,28 +18,6 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
     const db = client.db(databaseName)
 
-    // db.collection('users').findOne({ _id: new ObjectID("5ef34df645cfff1fd7338b36") }, (error, user) => {
-    //     if (error) {
-    //         console.log(error)
-    //     }
-
-    //     console.log(user)
-    // })
-
-    // db.collection('users').find({ name: 'Hung' }).toArray((error, users) => {
-    //     console.log(users)
-    // })
-
-    // db.collection('users').find({ name: 'Hung' }).count((error, count) => {
-    //     console.log(count)
-    // })
-
-    db.collection('tasks').findOne({ _id: new ObjectID('5ef34d02894afd1ea5c07926') }, (error, user) => {
-        console.log(user)
-    })
-
-    db.collection('tasks').find({ completed: true }).toArray((error, users) => {
-        console.log(users)
-    })
+    
 })
 
