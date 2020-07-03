5e7097495bf8c456c0eede2583a8c6871c5dddc0
Dang Viet Hung <dangviethung096@gmail.com>
2020-07-03 18:32:30 +0700

    update

---
+++ playground/9-async-await.js
@@ -0,0 +1,28 @@
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (a < 0 || b < 0) {
+                return reject('Negative value')
+            }
+
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+const doWork = async () => {
+    const sum4 = await add(-1, 1)
+    const sum = await add(1, 2)
+    const sum2 = await add(sum, 1)
+    const sum3 = await add(sum2, 1)
+
+    return sum3
+}
+
+doWork().then((result) => {
+    console.log('result ', result)
+}).catch((e) => {
+    console.log('e ', e)
+})
+
+
